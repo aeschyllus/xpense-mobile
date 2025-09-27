1d8fc8a6aae6a8dcc267706c198ada9933d0ed4a
aeschyllus <lapadamarkanthony@gmail.com>
2025-09-27 11:50:21 +0800

    chore: make `SafeAreaView` occupy the whole screen

--- utils/_hoc/withSafeArea.tsx
+++ utils/_hoc/withSafeArea.tsx
@@ -2,8 +2,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export const withSafeArea =
   <P extends object>(WrappedComponent: React.FC<P>) =>
-  (props: P) => (
-    <SafeAreaView>
-      <WrappedComponent {...props} />
-    </SafeAreaView>
-  );
+  (props: P) =>
+    (
+      <SafeAreaView style={{ flex: 1 }}>
+        <WrappedComponent {...props} />
+      </SafeAreaView>
+    );
