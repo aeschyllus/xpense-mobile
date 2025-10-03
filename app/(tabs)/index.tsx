a65c6e28359a1bbec0e5c4d8a3e17cf0b745f3e1
aeschyllus <lapadamarkanthony@gmail.com>
2025-10-03 22:14:29 +0800

    fix: resolve import error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { TransactionsScreen } from "@/pages/Transactions/Transactions";
+import { TransactionsScreen } from "@/pages/transactions/Transactions";
 import { withSafeArea } from "@/utils/_hoc/withSafeArea";
 
 export default withSafeArea(TransactionsScreen);
