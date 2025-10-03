c0a4a68e45ec060a7279b5052cb945884803f0ac
aeschyllus <lapadamarkanthony@gmail.com>
2025-10-03 22:15:32 +0800

    chore: leave note for future implementation

--- utils/_hoc/withSafeArea.tsx
+++ utils/_hoc/withSafeArea.tsx
@@ -1,10 +1,11 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 
+// NOTE: You can add a `backgroundColor` inside the `SafeAreaView` component.
+// This may be useful if you will implement a light/dark theme
 export const withSafeArea =
   <P extends object>(WrappedComponent: React.FC<P>) =>
-  (props: P) =>
-    (
-      <SafeAreaView style={{ flex: 1 }}>
-        <WrappedComponent {...props} />
-      </SafeAreaView>
-    );
+  (props: P) => (
+    <SafeAreaView style={{ flex: 1 }}>
+      <WrappedComponent {...props} />
+    </SafeAreaView>
+  );
