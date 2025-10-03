6934b1c63b77274654d9997cf996c73d4667987c
aeschyllus <lapadamarkanthony@gmail.com>
2025-10-03 23:40:22 +0800

    chore: add short form constant for months and day of week

---
+++ constants/date.ts
@@ -0,0 +1,23 @@
+export const DAY_OF_WEEK_SHORT = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+];
+
+export const MONTH_SHORT = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
