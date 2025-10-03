33ba85fc79cf96d030b7cd2304eb01c544f3afec
aeschyllus <lapadamarkanthony@gmail.com>
2025-10-03 23:38:57 +0800

    chore: update color values

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,7 +1,8 @@
 export const COLORS = {
   BLACK: "#1D1E18",
   WHITE: "#FFFFFF",
+  GRAY: "#5C6784",
   GREEN: "#45CB85",
-  RED: "#E88873",
-  BLUE: "#118AB2",
+  RED: "#EF2D56",
+  BLUE: "#016FB9",
 };
